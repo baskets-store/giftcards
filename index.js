c13e37c7e998e2f439dfb7402a1b92eb324e7bee
lauryngrootfaam <grootfaamlauryn@gmail.com>
2022-12-19 16:31:31 +0100

    latest changes

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-
-const btn = document.querySelector('[data-testid="table-pager-next"]')
-const table = document.querySelector('tbody')
+const btn = document.querySelector('[data-testid="table-pager-next"]');
+const table = document.querySelector("tbody");
 const tableArray = [];
+const exportData = [];
 
-const observer = new MutationObserver(function(){
-   console.log('hello')
-   tableArray.push(table)
-}); 
+const observer = new MutationObserver(function () {
+  console.log("hello");
+  tableArray.push(table);
+});
 
-const config = { childList: true};
+const config = { childList: true };
 
-
-
-async function getGiftcards (){
-
-if (btn.disabled == false){
-   await btn.click();
-    await console.log("btn is enabled")
-   observer.observe(table, config);
-   await  getGiftcards()
-}
-else {
-   await console.log("btn is disabled")
-}
-
+async function getGiftcards() {
+  if (btn.disabled == false) {
+    await btn.click();
+    await console.log("btn is enabled");
+    observer.observe(table, config);
+    await getGiftcards();
+    
+  } else {
+     console.log("btn is disabled");
+  }
 }
 
-getGiftcards()
+getGiftcards();
 
 
+tableArray.forEach((table) => {
+  table.children.forEach((child) => exportData.push({ child }));
+});
 
-
-
-
-observer.disconnect();
-
-
+await objectExporter({
+  exportable: exportData,
+  type: "xls",
+  fileName: "test",
+  headers: ["Cadeauboncode", "Activeringsdatum", "Status", "Order-ID", "	Saldo"],
+});
 
 //zolang de button niet disabled is wel je de table data pushen naar array
-
 //mutatioon observer checkt of er iets (children) veranderen op de pagina, als een child verandert wil je deze pushen naar array
+//observer.disconnect();
+
+// exportData.forEach(child => console.log(child.child.textContent))
+
+
