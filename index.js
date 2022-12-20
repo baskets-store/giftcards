e60df446a1c400fcaf17d6ba742b3040a3539dfb
lauryngrootfaam <grootfaamlauryn@gmail.com>
2022-12-20 14:43:27 +0100

    update

--- index.js
+++ index.js
@@ -1,108 +1,67 @@
+//script for export tool
+var script = document.createElement("script");
+script.src =
+  "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";
+document.getElementsByTagName("head")[0].appendChild(script);
+
 const btn = document.querySelector('[data-testid="table-pager-next"]');
 const table = document.querySelector("tbody");
 const tableArray = [];
 const exportData = [];
 
-const observer = new MutationObserver(function () {
-  console.log("hello");
-  tableArray.push(table);
-});
+//settings on what to observe
+const config = {
+  childList: true,
+};
 
-const config = { childList: true };
+//what to observe
+observer.observe(table, config);
 
+//function thats needs to take care of going through all the pages and stop when all the pages data is pushed to an array
 async function getGiftcards() {
-  if (btn.disabled == false) {
+      //first page table push to array
+      await tableArray.push(table);
+
+  if (btn.disabled == false) {    
+   //to next page
     await btn.click();
-    await console.log("btn is enabled");
-    observer.observe(table, config);
+
+    //anytime a child of the parent element (the table) changes than push the data from the table to an array. After that you want to go to the next page.
+    //there needs to change something first
+     const observer =  await new MutationObserver(function () {
+      console.log("hello observer ");
+    });
+
+     //repeat untill the button is disabled
     await getGiftcards();
-    
-  } else {
-     console.log("btn is disabled");
+   } 
+   
+   else {
+    console.log("btn is disabled");
   }
+
+  await tableArray.forEach((table) => {
+    table.children.forEach((child) =>
+      exportData.push({
+        child,
+      })
+    );
+  });
+
+  await objectExporter({
+    exportable: exportData,
+    type: "xls",
+    fileName: "test",
+    headers: [
+      "Cadeauboncode",
+      "Activeringsdatum",
+      "Status",
+      "Order-ID",
+      "	Saldo",
+    ],
+  });
 }
 
 getGiftcards();
 
-
-tableArray.forEach((table) => {
-  table.children.forEach((child) => exportData.push({ child}));
-});
-
-exportData.forEach(child => console.log(child.child.textContent))
-
-
-// await objectExporter({
-//   exportable: exportData,
-//   type: "xls",
-//   fileName: "test",
-//   headers: ["Cadeauboncode", "Activeringsdatum", "Status", "Order-ID", "	Saldo"],
-// });
-
-//zolang de button niet disabled is wel je de table data pushen naar array
-//mutatioon observer checkt of er iets (children) veranderen op de pagina, als een child verandert wil je deze pushen naar array
-//observer.disconnect();
-
 // exportData.forEach(child => console.log(child.child.textContent))
-
-        function tableToCSV() {
- 
-            // Variable to store the final csv data
-            // var csv_data = [];
- 
-            // Get each row data
-            var rows = document.getElementsByTagName('tr');
-            for (var i = 0; i < rows.length; i++) {
- 
-                // Get each column data
-                var cols = rows[i].querySelectorAll('td,th');
- 
-                // Stores each csv row data
-                var csvrow = [];
-                for (var j = 0; j < cols.length; j++) {
- 
-                    // Get the text data of each cell
-                    // of a row and push it to csvrow
-                    csvrow.push(cols[j].innerHTML);
-                }
- 
-                // Combine each column value with comma
-                exportData.push(csvrow.join(","));
-            }
- 
-            // Combine each row data with new line character
-            exportData = exportData.join('\n');
- 
-            // Call this function to download csv file 
-            downloadCSVFile(exportData);
- 
-        }
- 
-        function downloadCSVFile(exportData) {
- 
-            // Create CSV file object and feed
-            // our csv_data into it
-            CSVFile = new Blob([exportData], {
-                type: "text/csv"
-            });
- 
-            // Create to temporary link to initiate
-            // download process
-            var temp_link = document.createElement('a');
- 
-            // Download csv file
-            temp_link.download = "GfG.csv";
-            var url = window.URL.createObjectURL(CSVFile);
-            temp_link.href = url;
- 
-            // This link should not be displayed
-            temp_link.style.display = "none";
-            document.body.appendChild(temp_link);
- 
-            // Automatically click the link to
-            // trigger download
-            temp_link.click();
-            document.body.removeChild(temp_link);
-        }
-
-
