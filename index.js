595093839bf15747c3f8d6e4bb66dd6c002e302a
lauryngrootfaam <grootfaamlauryn@gmail.com>
2022-12-21 11:39:39 +0100

    latest version

--- index.js
+++ index.js
@@ -1,67 +1,56 @@
 //script for export tool
 var script = document.createElement("script");
 script.src =
-  "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";
+   "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";
 document.getElementsByTagName("head")[0].appendChild(script);
 
 const btn = document.querySelector('[data-testid="table-pager-next"]');
 const table = document.querySelector("tbody");
-const tableArray = [];
-const exportData = [];
+const giftcards = [];
 
-//settings on what to observe
 const config = {
-  childList: true,
+   childList: true,
 };
 
-//what to observe
-observer.observe(table, config);
+const observer = new MutationObserver(() => getGiftcards());
 
-//function thats needs to take care of going through all the pages and stop when all the pages data is pushed to an array
-async function getGiftcards() {
-      //first page table push to array
-      await tableArray.push(table);
+observer.observe(table, config)
 
-  if (btn.disabled == false) {    
-   //to next page
-    await btn.click();
-
-    //anytime a child of the parent element (the table) changes than push the data from the table to an array. After that you want to go to the next page.
-    //there needs to change something first
-     const observer =  await new MutationObserver(function () {
-      console.log("hello observer ");
-    });
-
-     //repeat untill the button is disabled
-    await getGiftcards();
-   } 
-   
+function getGiftcards() {
+ if(table.children.forEach((child, index) => child.contains(document.getElementsByTagName('span')[index])) ==  false) {
+   giftcards.push(table)
+   if (btn.disabled == false) {
+      nextPage();
+   }
    else {
-    console.log("btn is disabled");
-  }
-
-  await tableArray.forEach((table) => {
-    table.children.forEach((child) =>
-      exportData.push({
-        child,
-      })
-    );
-  });
-
-  await objectExporter({
-    exportable: exportData,
-    type: "xls",
-    fileName: "test",
-    headers: [
-      "Cadeauboncode",
-      "Activeringsdatum",
-      "Status",
-      "Order-ID",
-      "	Saldo",
-    ],
-  });
+      exportToCvc()
+   }
+}
+else {
+   console.log("skeleton")
+}
+  
 }
 
 getGiftcards();
 
-// exportData.forEach(child => console.log(child.child.textContent))
+
+function nextPage() {
+   btn.click();
+}
+
+
+async function exportToCvc() {
+   objectExporter({
+      exportable: giftcards,
+      type: "xls",
+      fileName: "test",
+      headers: [
+         "Cadeauboncode",
+         "Activeringsdatum",
+         "Status",
+         "Order-ID",
+         "Saldo",
+      ],
+   })
+}
